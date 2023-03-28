ab796119683ee6afa48f66a35cf6ccd19e5c9d84
THAIHOANH <125203796+THAIHOANH@users.noreply.github.com>
2023-03-28 16:07:41 +0700

    Add files via upload

---
+++ 1.3.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+struct OXY {
+public:
+    int x;
+    int y;
+};
+typedef struct Oxy ;
+
+void input(OXY* p) {
+    cout << "Nhap hoanh do: ";
+    cin >> p->x;
+    cout << "Nhap tung do: ";
+    cin >> p->y;
+}
+
+void output(OXY p) {
+    cout << "(" << p.x << ", " << p.y << ")";
+}
+
+int main() {
+    OXY A;
+    input(&A);
+    output(A);
+    return 0;
+}
