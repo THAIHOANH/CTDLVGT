758845abdeff4155fa90860cd5cdc544524bc685
THAIHOANH <125203796+THAIHOANH@users.noreply.github.com>
2023-04-04 17:16:32 +0700

    Add files via upload

---
+++ CTDL&GT_Stack_2.cpp
@@ -0,0 +1,7 @@
+// đây là kết quả xuất ra màn hình 
+Kích thước hiện tại của ngăn xếp là 1
+Kích thước hiện tại của ngăn xếp là 3
+Ngăn xếp đã đầy. Tình trạng tràn!
+Phần tử hàng đầu hiện tại trong ngăn xếp là 24
+Kích thước hiện tại của ngăn xếp là 0
+Ngăn xếp trống. Tình trạng chảy tràn!
